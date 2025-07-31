6b801c9bf20f49cb9e807def14d746849e800758
Basant Khaled <bosybasant589@gmail.com>
2025-07-31 04:49:43 +0300

    Update AreasOfShapes.dart

--- Functions/lib/AreasOfShapes.dart
+++ Functions/lib/AreasOfShapes.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 rectangle(int l , int w){
   return l*w;
 }
@@ -6,30 +7,35 @@ square(int s){
   return s*s;
 }
 circle(int r){
-  return 3.14*r*r;
+  return pi * r * r ;
 }
+void calculateArea(){
+  print("which shape do you want to calculate");
+  print("Rectangle or Square or Circle");
+  String shape = stdin.readLineSync()!.toLowerCase();
 
+  if (shape == "rectangle") {
+    print("Enter the length and width");
+    int l = int.parse(stdin.readLineSync()!);
+    int w = int.parse(stdin.readLineSync()!);
+    print("Area of rectangle is = ${rectangle(l, w)}");
+  }
+  else if (shape == "square") {
+    print("Enter the side of square");
+    int s = int.parse(stdin.readLineSync()!);
+    print("Area of square is = ${square(s)}");
+  }
+  else if (shape == "circle") {
+    print("Enter the radius of circle");
+    int r = int.parse(stdin.readLineSync()!);
+    print("Area of circle is = ${circle(r).toStringAsFixed(2)}");
+  }
+}
 main(){
-print("which shape do you want to calculate");
-print("1.Rectangle");
-print("2.Square");
-print("3.Circle");
-int choice=int.parse(stdin.readLineSync()!);
-if(choice==1){
-  print("Enter the length and width");
-  int l=int.parse(stdin.readLineSync()!);
-  int w=int.parse(stdin.readLineSync()!);
-  print("Area of rectangle is ${rectangle(l,w)}");
-}
-else if(choice==2){
-  print("Enter the side of square");
-  int s=int.parse(stdin.readLineSync()!);
-  print("Area of square is ${square(s)}");
-}
-else if(choice==3){
-  print("Enter the radius of circle");
-  int r=int.parse(stdin.readLineSync()!);
-  print("Area of circle is ${circle(r)}");
-}
-
+String answer = "";
+  do {
+  calculateArea();
+  stdout.write("Do you want to calculate again? (yes / no): ");
+  answer = stdin.readLineSync()!.toLowerCase();
+}while(answer=="yes");
 }
