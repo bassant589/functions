b5c87292dfdf971baa8a8c9ecd4eaa20b0361902
Basant Khaled <bosybasant589@gmail.com>
2025-07-31 04:30:40 +0300

    Update SimpleCalculator.dart

--- Functions/lib/SimpleCalculator.dart
+++ Functions/lib/SimpleCalculator.dart
@@ -1,42 +1,47 @@
 import 'dart:io';
 
-add(int x,int y){
-  return  x+y;
+add(double a,double b){
+  return a+b;
 }
-sub(int x,int y){
-  return x-y;
+sub(double a,double b){
+  return a-b;
 }
-mul(int x,int y){
-  return x*y;
+mul(double a,double b){
+  return a*b;
 }
-div(int x,int y){
-  return x~/y;
+div(double a,double b){
+  return a/b;
+}
+void calculate(double a, double b) {
+  stdout.write("Choose an operation (+, -, *, /): ");
+  String choice = stdin.readLineSync()!;
+  if (choice == '+') {
+    print("Result: $a + $b = ${add(a, b)}");
+  } else if (choice == '-') {
+    print("Result: $a - $b = ${sub(a, b)}");
+  } else if (choice == '*') {
+    print("Result: $a * $b = ${mul(a, b)}");
+  } else if (choice == '/') {
+    if (b == 0) {
+      print(" Error: Cannot divide by zero!");
+    } else {
+      print("Result: $a / $b = ${div(a, b)}");
+    }
+  } else {
+    print("Invalid operation.");
+  }
 }
 
 main(){
-  print("Enter two numbers ");
-  int x= int.parse(stdin.readLineSync()!);
-  int y= int.parse(stdin.readLineSync()!);
-
-  print("Choose your operation");
-  print("1 -> Addition");
-  print("2 -> Subtraction");
-  print("3 -> Multiplication");
-  print("4 -> Division");
-  int choice=int.parse(stdin.readLineSync()!);
-if(choice==1){
-  print("$x + $y = ${add(x,y)}");
-}
-else if(choice==2) {
-  print("$x - $y = ${sub(x, y)}");
-}
-else if(choice==3){
-  print("$x * $y = ${mul(x,y)}");
-}
-else if(choice==4){
-  print("$x / $y = ${div(x,y)}");
-}
-
+  String answer = "";
+  do{
+    print("Enter two numbers ");
+    double a= double.parse(stdin.readLineSync()!);
+    double b= double.parse(stdin.readLineSync()!);
+    calculate(a,b);
+    stdout.write("Do you want to calculate again? (yes / no): ");
+    answer = stdin.readLineSync()!.toLowerCase();
+  }while(answer=="yes");
 
 
 }
